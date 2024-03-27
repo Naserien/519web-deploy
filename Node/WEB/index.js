c4482b3e893cd708e4e5cfc0067a7442c3cb99d0
Naserien <1959483977@qq.com>
2024-03-27 12:56:13 -0400

    .

--- Node/WEB/index.js
+++ Node/WEB/index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const app = express();
-const {DefaultAzureCredential} = require('@azure/identity');
-const {SecretClient} = require('@azure/keyvault-secrets');
-
-
-const port = process.env.PORT || 8080
-
-const keyVaultName = "naserienkv1";
-const keyVaultUrl =`https://${keyVaultName}.vault.azure.net/`;
-const secretName = "Secret1";
-
-const credential = new DefaultAzureCredential();
-const client = new SecretClient(keyVaultUrl, credential);
-
-app.use('/', express.static('frontend/build'));
-
-app.get('/api', async (req, res) => {
-    //res.send(`Secret Value: ${process.env.KVSecret1}`);
-    res.send('Hello, world!');
-});
-
-app.listen(port,() =>{
-     console.log('Server listening on port '+ port);
-});
-
 // const express = require('express');
 // const app = express();
+// const {DefaultAzureCredential} = require('@azure/identity');
+// const {SecretClient} = require('@azure/keyvault-secrets');
+
+
 // const port = process.env.PORT || 3000
+
+// const keyVaultName = "naserienkv1";
+// const keyVaultUrl =`https://${keyVaultName}.vault.azure.net/`;
+// const secretName = "Secret1";
+
+// const credential = new DefaultAzureCredential();
+// const client = new SecretClient(keyVaultUrl, credential);
 
 // app.use('/', express.static('frontend/build'));
 
-// app.get('/api', (req, res) => {
-//   res.send('Hello, world!');
+// app.get('/api', async (req, res) => {
+//     //res.send(`Secret Value: ${process.env.KVSecret1}`);
+//     res.send('Hello, world!');
 // });
 
-// app.listen(port, () => {
-//   console.log('Server listening on port '+ port);
+// app.listen(port,() =>{
+//      console.log('Server listening on port '+ port);
 // });
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use('/', express.static('frontend/build'));
+
+app.get('/api', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log('Server listening on port '+ port);
+});
