bc9f4c418710b96800fc700e39aa76f07d92326c
Naserien Zeng <1959483977@qq.com>
2024-03-27 22:14:43 -0400

    Update test1.cs

--- C_Sharp/API1/test1.cs
+++ C_Sharp/API1/test1.cs
@@ -35,51 +35,54 @@
 //     }
 // }
 using System;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Functions.Worker.Http;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-public class QueueFunction
+namespace API1
 {
-    private readonly ILogger<QueueFunction> _logger;
-
-    public QueueFunction(ILogger<QueueFunction> logger)
+    public static class test1
     {
-        _logger = logger;
-    }
-
-    [FunctionName("test1")]
-    [QueueOutput("output-queue")]
-    public string[] Run([QueueTrigger("input-queue")] string myQueueItem, FunctionContext context)
-    {
-        _logger.LogInformation("C# queue trigger function processed a request.");
-
-        // Process the incoming message
-        // Example: Assume myQueueItem contains a JSON string representing an album
-        // You can deserialize it to an Album object and process it as needed
-        // For demonstration purposes, let's just log the incoming message
-        _logger.LogInformation($"Incoming message: {myQueueItem}");
-
-        // Use a string array to return more than one message.
-        string[] messages = {
-            $"Processed message: {myQueueItem}"
-        };
-
-        // Log the processed messages
-        foreach (var message in messages)
+        [FunctionName("test1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("api1", Connection = "AzureWebJobsStorage")] IAsyncCollector<string> queueCollector,
+            ILogger log)
         {
-            _logger.LogInformation(message);
-        }
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-        // Queue Output messages
-        return messages;
+            // Retrieve the second secret from environment variable
+            string secret2 = EnvironmentHelper.GetEnvironmentVariable("Secret2");
+
+            // Log the value of secret2
+            log.LogInformation($"Value of Secret 2: {secret2}");
+
+            // Generate a message to be inserted into the queue
+            string message = $"Value of Secret 2 from queue: {secret2}";
+
+            // Add the message to the storage queue
+            await queueCollector.AddAsync(message);
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string todayDate = DateTime.Today.ToString("yyyy-MM-dd");
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? $"This HTTP triggered function executed successfully on {todayDate}. Pass a name in the query string or in the request body for a personalized response. Message added to queue."
+                : $"Hello, {name}. This HTTP triggered function executed successfully on {todayDate}. Message added to queue.";
+
+            return new OkObjectResult(responseMessage);
+        }
     }
 }
+
 
 
