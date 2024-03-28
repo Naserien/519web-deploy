5d740c2fe4c0540355ca0f75343153b3f302ed16
Naserien Zeng <1959483977@qq.com>
2024-03-27 21:02:50 -0400

    Update test1.cs

--- C_Sharp/API1/test1.cs
+++ C_Sharp/API1/test1.cs
@@ -51,7 +51,7 @@ namespace API1
         [FunctionName("test1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [Queue("api1", Connection = "AzureWebJobsStorage")] IAsyncCollector<string> queueCollector,
+            [Queue("api1", Connection = "AzureWebJobsAzureWebJobsStorage")] IAsyncCollector<string> queueCollector,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
