4eb2f185e80efba759753499e248528728049319
Naserien Zeng <1959483977@qq.com>
2024-03-28 18:44:40 -0400

    Update index.js

--- Node/API2/QueueProcessor/index.js
+++ Node/API2/QueueProcessor/index.js
@@ -1,3 +1,4 @@
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    context.log('This is Secret 3:', process.env.secret3)
 };
