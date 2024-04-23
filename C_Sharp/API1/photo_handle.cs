daa723070daa46ed2fa97661dbfb769a4c26a902
Naserien <1959483977@qq.com>
2024-04-23 15:16:16 -0400

    add photo_handle in API1

---
+++ C_Sharp/API1/photo_handle.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace API1
+{
+    public static class photo_handle
+    {
+        [FunctionName("photo_handle")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [Queue("api1", Connection = "AzureWebJobsStorage")] IAsyncCollector<string> queueCollector,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Retrieve the second secret from environment variable
+            string secret2 = EnvironmentHelper.GetEnvironmentVariable("Secret2");
+
+            // Log the value of secret2
+            log.LogInformation($"Value of Secret 2: {secret2}");
+
+            // Generate a message to be inserted into the queue
+            string message = $"Value of Secret 2 from queue: {secret2}";
+
+            // Add the message to the storage queue
+            await queueCollector.AddAsync(message);
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string todayDate = DateTime.Today.ToString("yyyy-MM-dd");
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? $"This HTTP triggered function executed successfully on {todayDate}. Pass a name in the query string or in the request body for a personalized response. Message added to queue."
+                : $"Hello, {name}. This HTTP triggered function executed successfully on {todayDate}. Message added to queue.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
